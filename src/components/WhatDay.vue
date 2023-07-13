9c4f8af6c71199e3f91dd3c0e8a6524fe4daaef9
Aaron Maywood <am7274183@gmail.com>
2023-07-13 17:41:27 +0900

    Wikipediaへのリンク「貞観_(日本)」のパターンに対応

--- src/components/WhatDay.vue
+++ src/components/WhatDay.vue
@@ -20,22 +20,33 @@ fetch(url,{ mode: 'cors' })
     // articles.value = dekigoto.match(/\*.+\n/g);  // *の前に\が必要
     const tmp = dekigoto.match(/\*.+\n/g);  // *の前に\が必要
     for(let i=0;i<tmp.length;i++){ 
+      // Wikipedia記事にリンクを張る
       const regex = /\[\[([^\]]+)\]\]/g
       tmp[i] = tmp[i].replaceAll(regex,replacerfn)
+      // TODO 以下のテンプレート文字列を処理する
+      // TODO {{Cite ...}}→削除（引用元表記）
+      // TODO {{仮リンク|ハールレム攻城戦|en|Siege of Haarlem}}→「ハールレム攻城戦」
     }
     articles.value = tmp
   })
 
-// Wikipedia のリンクにする
+// リンク文字列 [[name]] をWikipedia のリンクにする
 // [[1787年]] → <a href="https://ja.wikipedia.org/wiki/1787%E5%B9%B4" target="_blank">1787年</a>
-// TODO 「貞観 (日本)|貞観」の場合には「貞観_日本」にする
 function replacerfn(match, p1){
-  console.log({
-    match: match,
-    patten:p1
-  })
-  const url = encodeURI(`https://ja.wikipedia.org/wiki/${p1}`)
-  return `<a href="${url}" target="_blank">${p1}</a>`
+  // 「貞観 (日本)|貞観」の場合にはwikipediaのURLを「貞観_(日本)」にし、リンク文字は「貞観」とする
+  let name = p1
+  let wikiname = p1
+  const regexp = /([^(]+) \(([^(]+)\)\|.*/  // 「貞観」「日本」に切り分ける
+  if(p1.match(regexp)){
+    const words = p1.match(regexp)
+    name = words[1]
+    wikiname = `${words[1]}_(${words[2]})`
+    console.log({
+    name:name,wikiname:wikiname})
+  }
+
+  const url = encodeURI(`https://ja.wikipedia.org/wiki/${wikiname}`)
+  return `<a href="${url}" target="_blank">${name}</a>`
 }
 
 
